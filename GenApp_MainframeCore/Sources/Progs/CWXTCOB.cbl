cdd315fab878865e8a2d4f1974b934b7634417a0
Xavier Eraso <xavier_eraso@bmc.com>
2025-10-16 13:26:56 +0200

    Maj Build

--- GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
+++ GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
@@ -14,7 +14,7 @@
       *                                                                *
       *  RUN JCL     - STORED IN XPEDITER/TSO SAMPLIB (CWXTJCLC)       *
       *                                                                *
-      * DEMO 2025.10.16 - 13:13                                        *
+      * DEMO 2025.10.16 - 13:26                                        *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
