c730d621ebf8b8e2a6954cf21db5b6190b2736bb
xerasox <eraso.xavier@gmail.com>
2025-12-11 16:31:42 +0100

    Maj commentaire

--- GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
+++ GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
@@ -14,7 +14,7 @@
       *                                                                *
       *  RUN JCL     - STORED IN XPEDITER/TSO SAMPLIB (CWXTJCLC)       *
       *                                                                *
-      * DEMO 2025.12.11 - 10:03                                        *
+      * DEMO 2025.12.11 - 16:31                                        *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
