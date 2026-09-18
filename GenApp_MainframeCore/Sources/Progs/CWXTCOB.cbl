2301062e089c7bbe2663fd546ae5bdfcab7d8301
xerasox <xavier_eraso@bmc.com>
2026-09-18 18:01:33 +0200

    test 3

--- GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
+++ GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
@@ -14,7 +14,7 @@
       *                                                                *
       *  RUN JCL     - STORED IN XPEDITER/TSO SAMPLIB (CWXTJCLC)       *
       *                                                                *
-      * DEMO 2026.09.18 - 17:45                                        *
+      * DEMO 2026.09.18 - 18:00                                        *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
