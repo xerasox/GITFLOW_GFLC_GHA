82a5302890878bf74d2e4685e3539361238bf5e6
xerasox <xavier_eraso@bmc.com>
2026-09-18 18:19:29 +0200

    fix test env ID

--- GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
+++ GenApp_MainframeCore/Sources/Progs/CWXTCOB.cbl
@@ -14,7 +14,7 @@
       *                                                                *
       *  RUN JCL     - STORED IN XPEDITER/TSO SAMPLIB (CWXTJCLC)       *
       *                                                                *
-      * DEMO 2026.09.18 - 18:03                                        *
+      * DEMO 2026.09.18 - 18:18                                        *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
